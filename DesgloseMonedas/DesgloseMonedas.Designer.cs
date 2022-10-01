e379fcdff146c1cd08a321e8fb294d39d780273f
Carlos Torres Graciano <carlosmiguel_torres@bhdleon.com.do>
2022-09-30 21:52:52 -0400

    first commit

---
+++ DesgloseMonedas/DesgloseMonedas.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace DesgloseMonedas
+{
+    partial class DesgloseMonedas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtBillete100 = new System.Windows.Forms.TextBox();
+            this.txtBillete50 = new System.Windows.Forms.TextBox();
+            this.txtMoneda25 = new System.Windows.Forms.TextBox();
+            this.txtMoneda10 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBillete500 = new System.Windows.Forms.TextBox();
+            this.txtBillete1000 = new System.Windows.Forms.TextBox();
+            this.txtBillete2000 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBillete200 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtMoneda5 = new System.Windows.Forms.TextBox();
+            this.txtMoneda1 = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cantidad";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(217, 37);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(151, 22);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // txtBillete100
+            // 
+            this.txtBillete100.Location = new System.Drawing.Point(235, 321);
+            this.txtBillete100.Name = "txtBillete100";
+            this.txtBillete100.ReadOnly = true;
+            this.txtBillete100.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete100.TabIndex = 3;
+            // 
+            // txtBillete50
+            // 
+            this.txtBillete50.Location = new System.Drawing.Point(235, 364);
+            this.txtBillete50.Name = "txtBillete50";
+            this.txtBillete50.ReadOnly = true;
+            this.txtBillete50.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete50.TabIndex = 4;
+            // 
+            // txtMoneda25
+            // 
+            this.txtMoneda25.Location = new System.Drawing.Point(235, 405);
+            this.txtMoneda25.Name = "txtMoneda25";
+            this.txtMoneda25.ReadOnly = true;
+            this.txtMoneda25.Size = new System.Drawing.Size(151, 22);
+            this.txtMoneda25.TabIndex = 5;
+            // 
+            // txtMoneda10
+            // 
+            this.txtMoneda10.Location = new System.Drawing.Point(235, 449);
+            this.txtMoneda10.Name = "txtMoneda10";
+            this.txtMoneda10.ReadOnly = true;
+            this.txtMoneda10.Size = new System.Drawing.Size(151, 22);
+            this.txtMoneda10.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Billetes de 100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Billetes de 50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Monedas de 25";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 449);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Monedas de 10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Billetes de 2000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Billetes de 1000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(97, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Billetes de 500";
+            // 
+            // txtBillete500
+            // 
+            this.txtBillete500.Location = new System.Drawing.Point(235, 235);
+            this.txtBillete500.Name = "txtBillete500";
+            this.txtBillete500.ReadOnly = true;
+            this.txtBillete500.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete500.TabIndex = 14;
+            // 
+            // txtBillete1000
+            // 
+            this.txtBillete1000.Location = new System.Drawing.Point(235, 189);
+            this.txtBillete1000.Name = "txtBillete1000";
+            this.txtBillete1000.ReadOnly = true;
+            this.txtBillete1000.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete1000.TabIndex = 15;
+            // 
+            // txtBillete2000
+            // 
+            this.txtBillete2000.Location = new System.Drawing.Point(235, 143);
+            this.txtBillete2000.Name = "txtBillete2000";
+            this.txtBillete2000.ReadOnly = true;
+            this.txtBillete2000.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete2000.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(97, 279);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Billetes de 200";
+            // 
+            // txtBillete200
+            // 
+            this.txtBillete200.Location = new System.Drawing.Point(235, 279);
+            this.txtBillete200.Name = "txtBillete200";
+            this.txtBillete200.ReadOnly = true;
+            this.txtBillete200.Size = new System.Drawing.Size(151, 22);
+            this.txtBillete200.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(97, 487);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 17);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Monedas de 5";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(97, 527);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 17);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Monedas de 1";
+            // 
+            // txtMoneda5
+            // 
+            this.txtMoneda5.Location = new System.Drawing.Point(235, 487);
+            this.txtMoneda5.Name = "txtMoneda5";
+            this.txtMoneda5.ReadOnly = true;
+            this.txtMoneda5.Size = new System.Drawing.Size(151, 22);
+            this.txtMoneda5.TabIndex = 21;
+            // 
+            // txtMoneda1
+            // 
+            this.txtMoneda1.Location = new System.Drawing.Point(235, 527);
+            this.txtMoneda1.Name = "txtMoneda1";
+            this.txtMoneda1.ReadOnly = true;
+            this.txtMoneda1.Size = new System.Drawing.Size(151, 22);
+            this.txtMoneda1.TabIndex = 22;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(203, 65);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(81, 33);
+            this.BtnCalcular.TabIndex = 23;
+            this.BtnCalcular.Text = "&Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(298, 65);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(88, 33);
+            this.BtnLimpiar.TabIndex = 24;
+            this.BtnLimpiar.Text = "&Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(83, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 102);
+            this.panel1.TabIndex = 25;
+            // 
+            // DesgloseMonedas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 568);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.txtMoneda1);
+            this.Controls.Add(this.txtMoneda5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtBillete200);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBillete2000);
+            this.Controls.Add(this.txtBillete1000);
+            this.Controls.Add(this.txtBillete500);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMoneda10);
+            this.Controls.Add(this.txtMoneda25);
+            this.Controls.Add(this.txtBillete50);
+            this.Controls.Add(this.txtBillete100);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "DesgloseMonedas";
+            this.Text = "Desglose de Monedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtBillete100;
+        private System.Windows.Forms.TextBox txtBillete50;
+        private System.Windows.Forms.TextBox txtMoneda25;
+        private System.Windows.Forms.TextBox txtMoneda10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBillete500;
+        private System.Windows.Forms.TextBox txtBillete1000;
+        private System.Windows.Forms.TextBox txtBillete2000;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBillete200;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtMoneda5;
+        private System.Windows.Forms.TextBox txtMoneda1;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DesgloseMonedas/DesgloseMonedas.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesgloseMonedas
+{
+    public partial class DesgloseMonedas : Form
+    {
+        int B2000, B1000, B500, B200, B100, B50, B25, B10, B5, B1, cantidad;
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                    this.txtCantidad.Focus();
+                }
+            }
+        }
+
+        public DesgloseMonedas()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               cantidad = int.Parse(txtCantidad.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar una cantidad Valida.");
+                return;
+            }
+
+            //Billetes de 2000
+            if (cantidad >= 2000)
+            {
+                B2000 = cantidad / 2000;
+                txtBillete2000.Text = B2000.ToString();
+                cantidad = cantidad - (B2000 * 2000);
+            }
+
+            //Billetes de 1000
+            if (cantidad >= 1000)
+            {
+                B1000 = cantidad / 1000;
+                txtBillete1000.Text = B1000.ToString();
+                cantidad = cantidad - (B1000 * 1000);
+            }
+
+            //Billetes de 500
+            if (cantidad >= 500)
+            {
+                B500 = cantidad / 500;
+                txtBillete500.Text = B500.ToString();
+                cantidad = cantidad - (B500 * 500);
+            }
+
+            //Billetes de 200
+            if (cantidad >= 200)
+            {
+                B200 = cantidad / 200;
+                txtBillete200.Text = B200.ToString();
+                cantidad = cantidad - (B200 * 200);
+            }
+
+            //Billetes de 100
+            if (cantidad >= 100)
+            {
+                B100 = cantidad / 100;
+                txtBillete100.Text = B100.ToString();
+                cantidad = cantidad - (B100 * 100);
+            }
+
+            //Billetes de 50
+            if (cantidad >= 50)
+            {
+                B50 = cantidad / 50;
+                txtBillete50.Text = B50.ToString();
+                cantidad = cantidad - (B50 * 50);
+            }
+
+            //Monedas de 25
+            if (cantidad >= 25)
+            {
+                B25 = cantidad / 25;
+                txtMoneda25.Text = B25.ToString();
+                cantidad = cantidad - (B25 * 25);
+            }
+
+            //Monedas de 10
+            if (cantidad >= 10)
+            {
+                B10 = cantidad / 10;
+                txtMoneda10.Text = B10.ToString();
+                cantidad = cantidad - (B10 * 10);
+            }
+
+            //Monedas de 5
+            if (cantidad >= 5)
+            {
+                B5 = cantidad / 5;
+                txtMoneda5.Text = B5.ToString();
+                cantidad = cantidad - (B5 * 5);
+            }
+
+            //Monedas de 1
+            if (cantidad >= 1)
+            {
+                B1 = cantidad / 1;
+                txtMoneda1.Text = B1.ToString();
+                cantidad = cantidad - (B1 * 1);
+            }
+        }
+    }
+}
